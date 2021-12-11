059cb5e3d2f81959d3294b4373f4e519a42995f8
Amjed Ali K <amjedmgm@gmail.com>
2021-12-11 15:02:11 +0530

    :recycle: Refactored Public Routes

--- pages/api/public/users/avatar-upload.ts
+++ pages/api/public/users/avatar-upload.ts
@@ -1,9 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as yup from "yup";
-import { UserType } from "../../../server/db";
-import { updateAvatarUser } from "../../../server/users";
-import { validation } from "../../../server/helper/validation";
+import { UserType } from "../../../../server/db";
+import { updateAvatarUser } from "../../../../server/users";
+import { validation } from "../../../../helpers/validation";
 
 const userValidationSchema: yup.SchemaOf<{}> = yup.object().shape({
   key: yup.string().required(),
@@ -23,8 +22,7 @@ export default async function handler(
       return res.status(400).json({ error: errors });
     }
     const user: UserType = { ...data } as unknown as UserType;
-    if (!user.avatar)
-      return res.status(400).json({ error: "Avatar is required" });
+    if (!user.avatar) return res.status(400).json({ error: "Avatar is required" });
     await updateAvatarUser(user.key, user.avatar);
 
     return res.status(200).json({
