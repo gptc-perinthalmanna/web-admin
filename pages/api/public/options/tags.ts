7570b9ee95ce7f1e8507f92baa30c03219fe8767
Amjed Ali K <amjedmgm@gmail.com>
2021-12-12 16:35:53 +0530

    :white_check_mark: Updated Tags

--- pages/api/public/options/tags.ts
+++ pages/api/public/options/tags.ts
@@ -1,35 +1,46 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const events = [
-  {name: "electronics-dept"},
-  {name: "electrical-dept"},
-  {name: "civil-dept"},
-  {name: "mechanical-dept"},
-  {name: "general-dept"},
-  {name: "office-section"},
-  {name: "nss"},
-  {name: "placement-cell"},
-  {name: "boomithraseena-club"},
-]
+  { name: "electronics-dept" },
+  { name: "electrical-dept" },
+  { name: "civil-dept" },
+  { name: "mechanical-dept" },
+  { name: "general-dept" },
+  { name: "office-section" },
+  { name: "nss" },
+  { name: "placement-cell" },
+  { name: "boomithraseena-club" },
+  { name: "staff-club" },
+];
 
 const notifications = [
-  {name: "electronics-dept"},
-  {name: "electrical-dept"},
-  {name: "civil-dept"},
-  {name: "mechanical-dept"},
-  {name: "general-dept"},
-  {name: "office-section"},
-  
-]
+  { name: "electronics-dept" },
+  { name: "electrical-dept" },
+  { name: "civil-dept" },
+  { name: "mechanical-dept" },
+  { name: "general-dept" },
+  { name: "office-section" },
+];
 
+const files = [
+  { name: "design-files" },
+  { name: "previous-question-paper" },
+  { name: "govt-order" },
+  { name: "downloads" },
+  { name: "extension-of-approval" },
+  { name: "academic-calendar" },
+  { name: "college-magazine" },
+];
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<{[key:string]: {name: string}[]} | { error: string }>
+  res: NextApiResponse<
+    { [key: string]: { name: string }[] } | { error: string }
+  >
 ) {
   if (req.method === "GET") {
     if (events) {
-      res.status(200).json({events, notifications});
+      res.status(200).json({ events, notifications, files });
     } else {
       return error(res);
     }
