ea0ff4cddd5aefe0eea400ba1b27cbc5a810153b
Amjed Ali K <amjedmgm@gmail.com>
2021-12-12 17:11:19 +0530

    :zap: Created Edit User Route

---
+++ pages/admin/users/edit/[id].js
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { toast } from "tailwind-toast";
+
+import Admin from "layouts/Admin.js";
+import Form from "components/Forms/Form";
+import { fetchData } from "helpers/fetcher";
+
+export default function EditDetails() {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data } = fetchData("/api/admin/users/" + id);
+  const formik = useFormik({
+    initialValues: {
+      name: data?.name,
+      email: data?.email,
+      designation: data?.designation,
+      department: data?.department,
+      phone: data?.phone,
+      address: data?.address,
+      facebook: data?.socialLinks?.facebook,
+      linkedin: data?.socialLinks?.linkedin,
+      instagram: data?.socialLinks?.instagram,
+      whatsapp: data?.socialLinks?.whatsapp,
+    },
+    enableReinitialize: true,
+    validationSchema: Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      designation: Yup.string().required(),
+      department: Yup.string().required(),
+      // role: Yup.array().of(Yup.string()),
+      phone: Yup.string().min(10).required(),
+      address: Yup.string().required(),
+
+      facebook: Yup.string().url(),
+      linkedin: Yup.string().url(),
+      instagram: Yup.string().url(),
+      whatsapp: Yup.string(),
+    }),
+    onSubmit: (values) => onSubmit(values, data),
+  });
+
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full px-4 py-3 mx-auto lg:w-8/12">
+          <EditForm formik={formik} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+EditDetails.layout = Admin;
+
+const onSubmit = (values, data) => {
+  axios
+    .post("/api/admin/users/edit/", {
+      ...values,
+      socialLinks: {
+        facebook: values?.facebook,
+        linkedin: values?.linkedin,
+        instagram: values?.instagram,
+        whatsapp: values?.whatsapp,
+      },
+      key: data.key,
+    })
+    .then((res) => {
+      toast()
+        .success("Great!", "Updated the details!")
+        .with({ color: "bg-blue-800" })
+        .from("bottom", "end")
+        .as("pill")
+        .show(); //show pill shaped toast
+      router.push("/admin/users");
+    })
+    .catch((err) => {
+      alert(err.response.data.message);
+    });
+};
+
+const EditForm = (formik) => (
+  <Form onSubmit={formik.handleSubmit}>
+    <Form.Title title="Edit User Profile">
+      <Form.Button />
+    </Form.Title>
+    <Form.Section title={"Basic Details"}>
+      <Form.TextInput {...formik.getFieldProps("name")} label="Full Name" />
+      {formik.touched.name && formik.errors.name ? (
+        <Form.Error>{formik.errors.name}</Form.Error>
+      ) : null}
+      <Form.TextInput {...formik.getFieldProps("email")} label="Email" />
+      {formik.touched.email && formik.errors.email ? (
+        <Form.Error>{formik.errors.email}</Form.Error>
+      ) : null}
+      <Form.TextInput {...formik.getFieldProps("phone")} label="Phone Number" />
+      {formik.touched.phone && formik.errors.phone ? (
+        <Form.Error>{formik.errors.phone}</Form.Error>
+      ) : null}
+      <Form.TextInput {...formik.getFieldProps("address")} label="Address" />
+      {formik.touched.address && formik.errors.address ? (
+        <Form.Error>{formik.errors.address}</Form.Error>
+      ) : null}
+    </Form.Section>
+    <Form.Section title={"Official Details"}>
+      <Form.TextInput
+        {...formik.getFieldProps("designation")}
+        label="Designation"
+      />
+      {formik.touched.designation && formik.errors.designation ? (
+        <Form.Error>{formik.errors.designation}</Form.Error>
+      ) : null}
+      <Form.TextInput
+        {...formik.getFieldProps("department")}
+        label="Department"
+      />
+      {formik.touched.department && formik.errors.department ? (
+        <Form.Error>{formik.errors.department}</Form.Error>
+      ) : null}
+    </Form.Section>
+    <Form.Section title={"Social Media"}>
+      <Form.TextInput
+        {...formik.getFieldProps("facebook")}
+        label="Facebook Link (Optional)"
+      />
+      {formik.touched.facebook && formik.errors.facebook ? (
+        <Form.Error>{formik.errors.facebook}</Form.Error>
+      ) : null}
+      <Form.TextInput
+        {...formik.getFieldProps("instagram")}
+        label="Instagram Link (Optional)"
+      />
+      {formik.touched.instagram && formik.errors.instagram ? (
+        <Form.Error>{formik.errors.instagram}</Form.Error>
+      ) : null}
+      <Form.TextInput
+        {...formik.getFieldProps("linkedin")}
+        label="LinkedIn Link (Optional)"
+      />
+      {formik.touched.linkedin && formik.errors.linkedin ? (
+        <Form.Error>{formik.errors.linkedin}</Form.Error>
+      ) : null}
+    </Form.Section>
+  </Form>
+);
